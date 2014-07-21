d2a85dc79a18895d85ca58c29c247ac33d505346
Mahdy Beygi <mahdy@beygi.org>
2014-07-22 00:15:33 +0430

    add mahfel.js

---
+++ scripts/mahfel.js
@@ -0,0 +1,26 @@
+// Description:
+//   hubot mahfel specified commands
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot noIdentify
+//
+// Author:
+//  Mahdy beygi
+
+
+module.exports = function(robot) {
+	robot.respond(/noIdentify/i, function(msg){
+		msg.reply("Please identify yourself via /msg NickServ identify <password>");
+	});
+	robot.respond(/hi/i, function(msg){
+		msg.reply("holla !");
+	});
+	
+	//register an
+}

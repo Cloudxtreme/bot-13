74c30bb284ba47f6e4466bbe03e51c70c9d4bd87
Mahdy Beygi <mahdy@beygi.org>
2014-07-24 15:14:30 +0430

    add TODO doc to mahfel.js

--- scripts/mahfel.js
+++ scripts/mahfel.js
@@ -24,5 +24,21 @@ module.exports = function(robot) {
 	robot.respond(/yo/i, function(msg){
 		msg.reply("yo");
 	});
-	//register an
+	//open vote for new user (all proUsers most be agree)
+	
+	//open vote for removing user (all proUsers most be agree except candidate)
+	
+	//register new user and give it "newUser" role
+	
+	/*hear for +1 and -1 for newUsers from proUsers and apply points to newUsers , also inform to channel
+	and if user point is greater than 99 give it "proUser" role
+	some controls most apply here : proUsers cant add or subtract more than 3 point for each newUsers per day
+	*/
+	
+	//update info (proUsers and newUsers can update their information (email_address,name,etc))
+	
+	//view info (every user can view info for every user including points)
+	
+	
+	
 };

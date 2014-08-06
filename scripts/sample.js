44659a97ab4d2e5bcf9eb9ad6e7cc01a4dacc7c8
Mahdy Beygi <mahdy@beygi.org>
2014-08-06 19:14:55 +0430

    add comment to sample

--- scripts/sample.js
+++ scripts/sample.js
@@ -20,13 +20,22 @@ module.exports = function(robot) {
     
     //respond method
     robot.respond(/hello/i, function(msg) {
+        //send reply
         msg.reply("hello World !");
     });
 
     //hear method
     robot.hear(/(\d+) smile/i, function(msg) {
+        
+        //access to regex matches using msg.match
         count = parseInt(msg.match[1].trim());
-        for(var i=1;i<=count;i++)
-            msg.send(":) ");
+        
+        var massage="";
+        //max smile is 20
+        for(var i=1;i<=count || i == 20 ;i++)
+            massage+=":) ";
+            
+        //send final message
+        msg.send(massage)
     });
 };
